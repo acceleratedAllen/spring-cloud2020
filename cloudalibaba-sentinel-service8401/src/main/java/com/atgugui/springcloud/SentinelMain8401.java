9e9e183f87ed075a1e209af63a74458458136615
Layyyyy <allen627992083@gmail.com>
2020-04-21 00:01:57 +0800

    sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atgugui/springcloud/SentinelMain8401.java
@@ -0,0 +1,17 @@
+package com.atgugui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author allen
+ * @date 2020-04-20 23:58
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class,args);
+    }
+}
